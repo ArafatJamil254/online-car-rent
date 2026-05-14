eee1805ccc0a9cca0d84e883e0af25e5f12cb0cf
Arafat Jamil <arafat.jamil32925@gmail.com>
2026-05-14 18:44:05 +0600

    Initial project folder structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Car Rent</title>
+</head>
+<body>
+
+<h1>Online Car Rent</h1>
+
+<a href="views/login.php">Login</a> |
+<a href="views/register.php">Register</a>
+
+</body>
+</html>
