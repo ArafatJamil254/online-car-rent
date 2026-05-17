41bee772e410dc04ccc656b76a554c33ef257bf3
sarar2003 <saifurtaher40@gmail.com>
2026-05-17 21:42:48 +0600

    fixed role issue in car_details

--- views/car_details.php
+++ views/car_details.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once(__DIR__ . '/../models/carModel.php');
-if(!isset($_SESSION['user_id']) || $_SESSION['role'] != 'member'){
+if(!isset($_SESSION['user_id']) ){
     header('location: login.php');
     exit;
 }
